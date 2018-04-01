ab776217051a0f236f8b11920624392bfce2d654
CR22 <carl_root776@esc.edu>
2018-04-01 09:20:21 -0400

    Add files via upload
    Added New StudentVersionPQ file for implementation of PriorityQueue in Main method.

--- Main.java
+++ Main.java
@@ -1,20 +1,38 @@
 package com.company;
 
+import java.util.PriorityQueue;
+
 public class Main {
 
     public static void main(String[] args) {
-        StudentVersionOne s1 = new StudentVersionOne("John","CS");
+        StudentVersionPQ s1 = new StudentVersionPQ("John","CS");
+        StudentVersionPQ s2 = new StudentVersionPQ("Al", "Math");
+        StudentVersionPQ s3 = new StudentVersionPQ("Beth", "Liberal");
+        
 	    s1.put(new Course("Course1", 100, 4, "A", "CS"));
         s1.put(new Course("Course2", 100, 3, "B+", "CS"));
         s1.put(new Course("Course3", 100, 2, "C", "CS"));
         s1.put(new Course("Course4", 100, 1, "D+", "CS"));
         s1.put(new Course("Course5", 100, 1, "D+", "Math"));
-        System.out.println(s1.getGPA());
+        
+        s2.put(new Course("Course6", 100, 3, "B+", "Liberal"));
+        
+        s3.put(new Course("Course7", 100, 4, "A", "Math"));
+//        System.out.println(s1.getGPA());
 //        System.out.println(s1.getCourse("Course1"));
 //        System.out.println("Course taken? " + s1.tookThisCourse("Course1"));
-        System.out.println("Total number of credits " + s1.getCreditCount());
-        System.out.println(s1.hasCompletedUpperLevelRequirements());
-        System.out.println("Eligible to graduate: " + s1.eligibleToGraduate());
+//        System.out.println("Total number of credits " + s1.getCreditCount());
+//        System.out.println(s1.hasCompletedUpperLevelRequirements());
+//        System.out.println("Eligible to graduate: " + s1.eligibleToGraduate());
+        
+        PriorityQueue<StudentVersionPQ> student = new PriorityQueue<StudentVersionPQ>();
+        
+        student.add(s1);
+        student.add(s2);
+        student.add(s3);
+       
+        while (!student.isEmpty())
+			System.out.println(student.remove());
 
     }
 
